83f9a39ec865d43e91b62da5abc639e759360709
muheserzhan <mrf00ntan@gmail.com>
2018-05-02 22:07:58 +0600

    to many server changes

---
+++ server/src/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors')
+
+const app = express()
+
+app.use(morgan('combined'))
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('/status', (req, res) => {
+  res.send({
+    message: 'hello world!'
+  })
+})
+
+app.listen(process.env.PORT || 8081)
